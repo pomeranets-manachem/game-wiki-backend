9a81d95ec138f0dd7921770e0ccda9712bee058e
Hugo Manachem <hugomanachem.pro@gmail.com>
2023-12-06 11:09:24 +0100

    initial Game Model

---
+++ models/Game.model.js
@@ -0,0 +1,32 @@
+const { Schema, model, default: mongoose } = require("mongoose");
+
+const gameSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required."],
+      unique: true
+    },
+    createdBy : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required : true
+    },
+    informations : {
+        type : String,
+        required : true
+    },
+    imageURL : {
+        type : String,
+        default : "https://static-00.iconduck.com/assets.00/no-image-icon-1024x1024-xg55mf9k.png"
+    }
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const Game = model("Game", gameSchema);
+
+module.exports = Game;
