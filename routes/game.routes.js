f4d59e4b9fc32c2ca950aca71ceff92dfeab3574
Hugo Manachem <hugomanachem.pro@gmail.com>
2023-12-07 15:01:49 +0100

    Read Game + related Categories

--- routes/game.routes.js
+++ routes/game.routes.js
@@ -74,14 +74,22 @@ router.get("/games/:gameId", (req, res) => {
 //GET /api/games/:gameId/categories
 router.get("/games/:gameId/categories", (req, res) => {
   const {gameId} = req.params;
-  Category.find({"games" : gameId})
-    .then((categories) => {
-      res.status(200).json(categories);
-    })
+  Game.findOne({"_id" : gameId})
+    .then((game) => {
+      Category.find({"games" : gameId})
+      .then((categories) => {
+        res.status(200).json({game,categories});
+      })
+      .catch((err) => {
+        res.status(500).json({message : err.message});
+        console.log(err);
+      });
+      })
     .catch((err) => {
       res.status(500).json({message : err.message});
       console.log(err);
     });
+  
 });
 
 //PUT /api/games/:gameId
