4a75e077a6827079c35b2a2ac5780ff345eab516
Hugo Manachem <hugomanachem.pro@gmail.com>
2023-12-08 15:01:50 +0100

    Add commentSchema to game Model

--- models/Game.model.js
+++ models/Game.model.js
@@ -1,5 +1,21 @@
 const { Schema, model, default: mongoose } = require("mongoose");
 
+const commentSchema = new Schema(
+  { 
+    comment : {
+      type : String,
+      required : [true, "You need to write something"]
+    },
+    author : {
+      type : mongoose.Schema.Types.ObjectId,
+       ref: "User"
+    } 
+  },
+  {
+    timestamps: true,
+  }
+);
+
 const gameSchema = new Schema(
   {
     name: {
@@ -14,7 +30,8 @@ const gameSchema = new Schema(
     imageURL : {
         type : String,
         default : "https://static-00.iconduck.com/assets.00/no-image-icon-1024x1024-xg55mf9k.png"
-    }
+    },
+    comments : [commentSchema]
   },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
